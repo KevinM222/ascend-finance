c09af50c69b1d194fba143071ff1473e255eeb46
Kevin Mandracchia <kmandracchia@bnl.gov>
2025-01-13 17:13:00 -0500

    commit change trying to integrate working js files

--- assets/Js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const rocket = document.querySelector('.rocket');
-    const sections = document.querySelectorAll('.timeline-section');
-
-    window.addEventListener('scroll', () => {
-        const scrollPosition = window.scrollY;
-        rocket.style.transform = `translateY(-${scrollPosition}px)`;
-    });
-});
