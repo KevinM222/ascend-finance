6ad6e4d32da9a5b79126fa09885474681ebb28ac
Kevin Mandracchia <kmandracchia@bnl.gov>
2025-02-02 14:49:11 -0500

    commit

--- AscendDEX/scripts/deploy_ascRewards.js
+++ AscendDEX/scripts/deploy_ascRewards.js
@@ -1,7 +1,7 @@
 // scripts/deploy_ascRewards.js
 
 async function main() {
-    // Get deployer account
+    // Get deployer account from Hardhat
     const [deployer] = await ethers.getSigners();
     console.log("Deploying AscRewards with account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
@@ -9,13 +9,14 @@ async function main() {
     // Set the ASC token contract address (update if necessary)
     const ascTokenAddress = "0x4456B0F017F6bF9b0aa7a0ac3d3F224902a1937A";
   
-    // Define the pre-allocated reward pool: 100,000,000 ASC (18 decimals)
+    // Define the pre-allocated reward pool for rewards: 100,000,000 ASC (with 18 decimals)
     const rewardPool = ethers.utils.parseUnits("100000000", 18);
   
     // Get the contract factory for AscRewards
     const AscRewards = await ethers.getContractFactory("AscRewards");
   
-    // Deploy the AscRewards contract with the ASC token address and reward pool
+    // Deploy the AscRewards contract with constructor parameters:
+    // ascTokenAddress and rewardPool
     const ascRewards = await AscRewards.deploy(ascTokenAddress, rewardPool);
     await ascRewards.deployed();
   
