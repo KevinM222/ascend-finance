bd7b25d894cf3c3f6aba79f092a12b79ae5785eb
Kevin Mandracchia <kmandracchia@bnl.gov>
2025-02-04 20:10:15 -0500

    com

--- AscendDEX/loadContract.js
+++ AscendDEX/loadContract.js
@@ -8,6 +8,7 @@ const rewardsAddress = "0xa2D979bF900C1Ccf153A2Ba6BB249B9e85a95690";
 // Read the ABI from the local file
 const rewardsABI = JSON.parse(fs.readFileSync(path.resolve('../Ascend/AscRewardsABI.json')));
 
+
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 const rewardsContract = new ethers.Contract(rewardsAddress, rewardsABI.abi, signer);
