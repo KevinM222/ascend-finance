dd36c7ec8afbde66187184192938cc9232d445d6
Kevin Mandracchia <kmandracchia@bnl.gov>
2025-02-04 20:14:22 -0500

    commit

--- AscendDEX/loadContract.js
+++ AscendDEX/loadContract.js
@@ -1,4 +1,3 @@
-// loadContract.js
 import { ethers } from 'ethers';
 import fs from 'fs';  // Add fs module to read local files
 import path from 'path';  // Use path module to handle file paths
@@ -6,9 +5,7 @@ import path from 'path';  // Use path module to handle file paths
 const rewardsAddress = "0xa2D979bF900C1Ccf153A2Ba6BB249B9e85a95690";
 
 // Read the ABI from the local file
-const path = require('path');
 const rewardsABI = JSON.parse(fs.readFileSync(path.resolve('../Ascend/frontend/AscRewardsABI.json')));
-
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
