5e2878db4c9f0643dfd9a8f4a98ec041e7f53afd
Kevin Mandracchia <kmandracchia@bnl.gov>
2025-02-04 20:15:55 -0500

    commit

--- AscendDEX/loadContract.js
+++ AscendDEX/loadContract.js
@@ -4,8 +4,8 @@ import path from 'path';  // Use path module to handle file paths
 
 const rewardsAddress = "0xa2D979bF900C1Ccf153A2Ba6BB249B9e85a95690";
 
-// Read the ABI from the local file
-const rewardsABI = JSON.parse(fs.readFileSync(path.resolve('../Ascend/frontend/AscRewardsABI.json')));
+// Adjust the path to the correct file location
+const rewardsABI = JSON.parse(fs.readFileSync(path.resolve('../frontend/AscRewardsABI.json')));
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
